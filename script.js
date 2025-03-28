78c51bc9c82618e22b1149034485e16c5f11c434
Ana Caroline <140931809+Anatskk@users.noreply.github.com>
2025-03-28 16:58:36 -0300

    Responsividade do site

--- script.js
+++ script.js
@@ -14,3 +14,31 @@ document.addEventListener('DOMContentLoaded', function () {
 function toggleMenu() {
     document.getElementById("overlay").classList.toggle("show");
 }
+
+document.querySelectorAll('*').forEach(el => {
+    if (el.scrollWidth > document.documentElement.clientWidth) {
+        console.log('Elemento causando overflow:', el);
+    }
+});
+
+// Verifica se o primeiro radio button está marcado
+const radio1 = document.getElementById('radio1');
+const vejaMaisBtn = document.querySelector('.veja-mais');
+
+// Função que mostra o botão "VEJA MAIS" quando a primeira imagem está visível
+function verificarSlider() {
+    if (radio1.checked) {
+        vejaMaisBtn.style.display = 'block'; // Mostra o botão
+    } else {
+        vejaMaisBtn.style.display = 'none'; // Esconde o botão
+    }
+}
+
+// Adiciona um evento para verificar a mudança no slider
+document.querySelectorAll('.manual-btn').forEach((btn) => {
+    btn.addEventListener('click', verificarSlider);
+});
+
+// Verifica o estado inicial
+verificarSlider();
+
