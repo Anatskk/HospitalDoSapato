27118ad3c0d8cb9baa1b495ed61d6f6bddb69403
Ana Caroline <140931809+Anatskk@users.noreply.github.com>
2025-03-30 15:03:34 -0300

    Parte de serviços feito

--- script.js
+++ script.js
@@ -38,3 +38,58 @@ document.querySelectorAll('.manual-btn').forEach((btn) => {
 
 verificarSlider();
 
+let currentIndex1 = 0;
+const slider1 = document.getElementById("slider2");
+const totalCards1 = document.querySelectorAll("#slider2 .card").length;
+const cardWidth1 = 250; 
+const visibleCards = 3; 
+
+let currentIndex2 = 0;
+const slider2 = document.getElementById("slider4");
+const totalCards2 = document.querySelectorAll("#slider4 .card3").length;
+
+function updateSlider(slider, currentIndex) {
+    const offset = -(currentIndex * cardWidth1);
+    slider.style.transform = `translateX(${offset}px)`;
+}
+
+function nextSlide(sliderIndex) {
+    if (sliderIndex === 1) {
+        if (currentIndex1 < totalCards1 - visibleCards) {
+            currentIndex1++;
+        } else {
+            currentIndex1 = 0; 
+        }
+        updateSlider(slider1, currentIndex1);
+    } else if (sliderIndex === 2) {
+        if (currentIndex2 < totalCards2 - visibleCards) {
+            currentIndex2++;
+        } else {
+            currentIndex2 = 0;
+        }
+        updateSlider(slider2, currentIndex2);
+    }
+}
+
+function prevSlide(sliderIndex) {
+    if (sliderIndex === 1) {
+        if (currentIndex1 > 0) {
+            currentIndex1--;
+        } else {
+            currentIndex1 = totalCards1 - visibleCards; 
+        }
+        updateSlider(slider1, currentIndex1);
+    } else if (sliderIndex === 2) {
+        if (currentIndex2 > 0) {
+            currentIndex2--;
+        } else {
+            currentIndex2 = totalCards2 - visibleCards;
+        }
+        updateSlider(slider2, currentIndex2);
+    }
+}
+
+setInterval(function() {
+    nextSlide(1);  
+    nextSlide(2);  
+}, 5000);
